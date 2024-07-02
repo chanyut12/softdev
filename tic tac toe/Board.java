73a601b2a1c60a965550d0174aa73facda5be1c2
Chanyut Lisawat <66160295@go.buu.ac.th>
2024-07-02 08:36:47 +0700

    first commit

---
+++ tic tac toe/Board.java
@@ -0,0 +1,71 @@
+public class Board {
+    private char[][] board;
+    private static final int SIZE = 3;
+
+    public Board() {
+        board = new char[SIZE][SIZE];
+        initializeBoard();
+    }
+
+    public void initializeBoard() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                board[i][j] = '-';
+            }
+        }
+    }
+
+    public void printBoard() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                System.out.print(board[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public boolean isFull() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                if (board[i][j] == '-') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public boolean placeMark(int row, int col, char symbol) {
+        if (row >= 0 && col >= 0 && row < SIZE && col < SIZE && board[row][col] == '-') {
+            board[row][col] = symbol;
+            return true;
+        }
+        return false;
+    }
+
+    public boolean checkWin(char symbol) {
+        // Check rows
+        for (int i = 0; i < SIZE; i++) {
+            if (board[i][0] == symbol && board[i][1] == symbol && board[i][2] == symbol) {
+                return true;
+            }
+        }
+
+        // Check columns
+        for (int i = 0; i < SIZE; i++) {
+            if (board[0][i] == symbol && board[1][i] == symbol && board[2][i] == symbol) {
+                return true;
+            }
+        }
+
+        // Check diagonals
+        if (board[0][0] == symbol && board[1][1] == symbol && board[2][2] == symbol) {
+            return true;
+        }
+        if (board[0][2] == symbol && board[1][1] == symbol && board[2][0] == symbol) {
+            return true;
+        }
+
+        return false;
+    }
+}
---
+++ tic tac toe/Game.java
@@ -0,0 +1,91 @@
+import java.util.Scanner;
+
+public class Game {
+    private Board board;
+    private Player player1;
+    private Player player2;
+    private Player currentPlayer;
+    private int player1Wins, player1Losses, player1Draws;
+    private int player2Wins, player2Losses, player2Draws;
+    private Scanner scanner;
+    private boolean gameRunning;
+
+    public Game() {
+        board = new Board();
+        player1 = new Player('X');
+        player2 = new Player('O');
+        currentPlayer = player1;
+        player1Wins = player1Losses = player1Draws = 0;
+        player2Wins = player2Losses = player2Draws = 0;
+        scanner = new Scanner(System.in);
+        gameRunning = true;
+    }
+
+    public void start() {
+        while (gameRunning) {
+            board.printBoard();
+            System.out.println("Player " + currentPlayer.getSymbol() + ", enter your move (row and column) or 'q' to quit:");
+            String input = scanner.nextLine();
+
+            if (input.equalsIgnoreCase("q")) {
+                System.out.println("Exiting the game...");
+                gameRunning = false;
+                continue;
+            }
+
+            String[] move = input.split(" ");
+            if (move.length != 2) {
+                System.out.println("Invalid input. Please try again.");
+                continue;
+            }
+
+            int row, col;
+            try {
+                row = Integer.parseInt(move[0]);
+                col = Integer.parseInt(move[1]);
+            } catch (NumberFormatException e) {
+                System.out.println("Invalid input. Please try again.");
+                continue;
+            }
+
+            if (board.placeMark(row - 1, col - 1, currentPlayer.getSymbol())) {
+                boolean gameWon = board.checkWin(currentPlayer.getSymbol());
+                if (gameWon) {
+                    board.printBoard();
+                    System.out.println("Player " + currentPlayer.getSymbol() + " wins!");
+                    updateScores(currentPlayer);
+                    printScores();
+                } else if (board.isFull()) {
+                    board.printBoard();
+                    System.out.println("The game is a tie!");
+                    updateScores(null);
+                    printScores();
+                } else {
+                    currentPlayer = (currentPlayer == player1) ? player2 : player1;
+                }
+            } else {
+                System.out.println("This move is not valid");
+            }
+        }
+
+        scanner.close();
+    }
+
+    private void updateScores(Player winner) {
+        if (winner == player1) {
+            player1Wins++;
+            player2Losses++;
+        } else if (winner == player2) {
+            player2Wins++;
+            player1Losses++;
+        } else {
+            player1Draws++;
+            player2Draws++;
+        }
+    }
+
+    private void printScores() {
+        System.out.println("Player X: Wins: " + player1Wins + ", Losses: " + player1Losses + ", Draws: " + player1Draws);
+        System.out.println("Player O: Wins: " + player2Wins + ", Losses: " + player2Losses + ", Draws: " + player2Draws);
+    }
+}
